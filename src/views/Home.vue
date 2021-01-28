b390ead5c0b6584c0614733f6c815f72027fc78e
俊正我 <69614773+Junzhengwo1@users.noreply.github.com>
2021-01-28 20:17:34 +0800

    菜单列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
     <div>
-        Home
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <!--启用路由模式-->
+                    <el-menu router >
+                        <el-submenu index="1" v-for="(item,index) in this.$router.options.routes"
+                                    :key="index"
+                                    v-if="!item.hidden" >
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>{{item.name}}</span>
+                            </template>
+                                <el-menu-item
+                                        :index="children.path"
+                                        v-for="(children,indexj) in item.children">
+                                        {{children.name}}
+                                </el-menu-item>
+                        </el-submenu >
+                    </el-menu>
+                </el-aside>
+                <el-container>
+                    <el-main>
+                        <router-view>
+
+                        </router-view>
+
+                    </el-main>
+                    <el-footer>Footer</el-footer>
+                </el-container>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
     export default {
         name: "Home"
+
     }
 </script>
 
-<style scoped>
+<style>
 
 </style>
