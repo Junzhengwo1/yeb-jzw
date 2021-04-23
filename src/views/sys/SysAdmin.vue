f4f4d6a3b56f8a8cac309c3ee21820f2c5961a67
McQueen <soslovemiss@dingtalk.com>
2021-04-23 22:25:30 +0800

    操作员更新角色操作

--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -35,7 +35,23 @@
                     <div>
                         用户角色：
                         <el-tag style="margin-right: 4px;color: #783008" type="success" v-for="(role,indexj) in admin.roles" :key="indexj">{{role.nameZh}}</el-tag>
-                        <el-button type="text" icon="el-icon-more"></el-button>
+                        <el-popover
+                                placement="right"
+                                title="角色列表"
+                                width="200"
+                                @show="showPop(admin)"
+                                @hide="hidePop(admin)"
+                                trigger="click">
+                            <el-select v-model="selectRoles" multiple placeholder="请选择">
+                                <el-option
+                                        v-for="(r,index) in allRoles"
+                                        :key="index"
+                                        :label="r.nameZh"
+                                        :value="r.id">
+                                </el-option>
+                            </el-select>
+                            <el-button slot="reference" type="text" icon="el-icon-more"></el-button>
+                        </el-popover>
                     </div>
                     <div>
                         备注：{{admin.remark}}
@@ -48,19 +64,76 @@
 </template>
 
 <script>
+    import {getRequest} from "../../utils/api";
+
     export default {
         name: "SysAdmin",
         data(){
             return{
                 admins:[],
                 //关键字来查询用的
-                keywords:''
+                keywords:'',
+                allRoles:[],
+                selectRoles:[]
             }
         },
         mounted() {
             this.initAdmins();
         },
         methods:{
+            /**
+             * 重点代码
+             * @param admin
+             */
+            hidePop(admin){
+                let roles=[];
+                Object.assign(roles,admin.roles);
+                let flag=false;
+                if(roles.length!=this.selectRoles.length){
+                    flag=true;
+                }else {
+                    for(let i=0;i<roles.length ;i++){
+                        let role=roles[i];
+                        for(let j=0;j<this.selectRoles.length;j++){
+                            let sr= this.selectRoles[j];
+                            if(role.id==sr){
+                                roles.splice(i,1);
+                                i--;
+                                break;
+                            }
+                        }
+                    }
+                    if(roles.length!=0){
+                        flag=true;
+                    }
+                }
+                if(flag){
+                    let url='/system/admin/role?adminId=' + admin.id;
+                    this.selectRoles.forEach(sr=>{
+                        url += '&rids='+sr;
+                    });
+                    this.putRequest(url).then(resp=>{
+                        if(resp){
+                            this.initAdmins();
+                        }
+                    })
+                }
+            },
+            showPop(admin){
+                this.initAllRoles();
+                let roles=admin.roles;
+                this.selectRoles=[];
+                roles.forEach(r=>{
+                    this.selectRoles.push(r.id);
+                })
+            },
+            initAllRoles(){
+                getRequest('/system/admin/roles').then(resp=>{
+                  if(resp){
+                      this.allRoles=resp;
+                  }
+                })
+            },
             enabledChange(admin){
                 this.putRequest('/system/admin/',admin).then(resp=>{
                     if(resp){
